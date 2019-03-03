0a94b1a4a91e71d880c6a79e72c31bc59134d64f
William Campos <william_apr16@hotmail.com>
2019-03-03 15:20:58 -0300

    Primeiro Commit

---
+++ WCFExerciciosCliente/WCFExerciciosCliente/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WCFExerciciosCliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvExercicios = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvExercicios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvExercicios
+            // 
+            this.dgvExercicios.AllowUserToAddRows = false;
+            this.dgvExercicios.AllowUserToDeleteRows = false;
+            this.dgvExercicios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvExercicios.Location = new System.Drawing.Point(63, 48);
+            this.dgvExercicios.Name = "dgvExercicios";
+            this.dgvExercicios.ReadOnly = true;
+            this.dgvExercicios.Size = new System.Drawing.Size(481, 219);
+            this.dgvExercicios.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvExercicios);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvExercicios)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvExercicios;
+    }
+}
+
